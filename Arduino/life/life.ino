9ac1b98728b18d4e02a9193df98a90f02d68cb0e
robert <you@example.com>
2025-10-11 12:28:02 +0100

    Added mostly finished conway's game of life

--- Arduino/life/life.ino
+++ Arduino/life/life.ino
@@ -1,4 +1,10 @@
 
+/*
+LIFE
+A rudimentary implimentation of conway's game of life on the Max7219 display
+Written by Robert Jordan
+*/
+
 //import libarries
 #include <MD_MAX72xx.h>
 #include <MD_Parola.h>
@@ -11,16 +17,17 @@
 #define CS_PIN 3
 #define SCREEN_WIDTH 32
 #define SCREEN_HEIGHT 8
+#define SCREEN_SIZE SCREEN_WIDTH*SCREEN_HEIGHT
 
 MD_MAX72XX ds = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 //MD_Parola text_ds = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-//create the initial setup for the game of life
-const bool state[SCREEN_HEIGHT][SCREEN_WIDTH] = {
-{1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
-{1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
-{1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
-{1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
+//create the initial setup for the game
+bool state[SCREEN_HEIGHT][SCREEN_WIDTH] = {
+{1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
+{1,1,1,0,0,0,1,1,1,1,1,0,1,0,1,1,0,0,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
+{1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,0,0,1,1,1,1,1,0,1,0,1,1},
+{1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
 {1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
 {1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
 {1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1},
@@ -28,7 +35,7 @@ const bool state[SCREEN_HEIGHT][SCREEN_WIDTH] = {
 };
 
 //simulation parameters
-#define TICK_SPEED 200
+#define TICK_SPEED 80
 
 void setup() {
   Serial.begin(9600);
@@ -37,17 +44,65 @@ void setup() {
   ds.clear();
 }
 
-void draw_state(){
+void update_state(){
   //draw the current state of the simulation
   for (int y = 0; y < SCREEN_HEIGHT; y++){
     for (int x = 0; x < SCREEN_WIDTH; x++){
-      ds.setPoint(y, x, state[y][x]);
+      //only draw live cells
+      if (state[y][x]){
+        ds.setPoint(y, x, 1);
+      }
+      //update cells
+      run_rules(x, y);
     }
   }
 }
 
+bool check_cell(int x, int y){
+  //first, check the cell is not out of bounds
+  if ((x >= SCREEN_WIDTH || x < 0) || (y >= SCREEN_HEIGHT || y < 0)){
+    return 0;
+  }
+  //otherwise, return it's value
+  return state[y][x];
+}
+
+int count_adjacent(int x, int y){
+  //count all of the surrounding cells in a clockwise pattern
+  int count = 0;
+  count += check_cell(x-1, y+1); //top-left
+  count += check_cell(x, y+1); //top-middle
+  count += check_cell(x+1, y+1); //top-right
+  count += check_cell(x+1, y); //middle-right
+  count += check_cell(x+1, y-1); //bottom-right
+  count += check_cell(x, y-1); //bottom-middle
+  count += check_cell(x-1, y-1); //bottom-left
+  count += check_cell(x-1, y); //middle-left
+  return count;
+}
+
+void run_rules(int x, int y){
+  //get number of adjacent cells and the cell's state
+  int count = count_adjacent(x, y);
+
+  //1. Any live cell with fewer than two live neighbours dies
+  if (state[y][x] && count < 2){
+    state[y][x] = 0;
+  }
+  //2. Any live cell with two or three live neighbours lives on
+  //3. Any live cell with more than three live neighbours dies
+  if (state[y][x] && count > 3){
+    state[y][x] = 0;
+  }
+  //4. Any dead cell with exactly three live neighbours becomes a live cell
+  if (!state[y][x] && count == 3){
+    state[y][x] = 1;
+  }
+}
+
 void loop() {
-  draw_state();
+  update_state();
+  count_adjacent(1,6);
   delay(TICK_SPEED);
   ds.clear();
 }
